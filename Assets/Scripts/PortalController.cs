f014f2b5c87900287098cd484634b68c476d3091
ajgp8821 <ajgp8821@gmail.com>
2022-03-24 23:21:18 -0300

    Saving Game's State

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -11,6 +11,7 @@ public class PortalController : CollectableController {
         // base.OnCollide(coll);
         if (coll.name == "Player") {
             // Teleport the player
+            GameManager.instance.SaveState();
             string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
             SceneManager.LoadScene(sceneName);
         }
